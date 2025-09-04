80151fc9dc85c75572f8e1304843e8f1d163371c
ayane1118 <183798921+ayane1118@users.noreply.github.com>
2025-09-04 12:19:57 +0900

    ・MainActivity5にてWebAPIへアクセス機能の実装 ・MainActivity5アスキーアート標示機能実装 ・Gist.java作成

---
+++ app/src/main/java/jp/ac/meijo/android/s241205023/Gist.java
@@ -0,0 +1,10 @@
+package jp.ac.meijo.android.s241205023;
+
+import java.util.Map;
+
+public class Gist {
+    public Map<String, GistFile> files;
+    public static class GistFile {
+        public String content;
+    }
+}
